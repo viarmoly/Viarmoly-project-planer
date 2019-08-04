ef4faf2f3ee8dc7c71adcb4ba96a90e168c7e019
viarmoly <v.iarmolitskyi@gmail.com>
2019-08-04 23:15:49 +0300

    finnished at lesson 5

---
+++ src/components/layout/SignedLinks.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom'
+
+const SignInLinks = () => {
+    return (
+        <ul className='right'>
+            <li><NavLink to='/'>New Project</NavLink></li>
+            <li><NavLink to='/'>Log out</NavLink></li>
+            <li><NavLink to='/' className='btn btn-floating pink lighten-1'>IV</NavLink></li>
+        </ul>
+    )
+};
+
+export default SignInLinks;
